ccefad8b6fb7e64eddb7e6767900f25e10dc742d
Sana-maraym90 <sanamaryam6677@gmail.com>
2024-09-02 13:37:39 +0500

    Manage when player moves out of left side of screen and when it moves too slow

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rb;
-
+    private int leftExtreme = -15;
     public bool isLaunched =false;
 
     private void Awake()
@@ -13,18 +13,42 @@ public class Player : MonoBehaviour
     }
     private void Update()
     {
-        if (isLaunched && (rb.IsSleeping() || rb.velocity.magnitude < 0.1f || transform.position.x > 200))
+        if (isLaunched && (rb.IsSleeping() || rb.velocity.magnitude < 0.1f || transform.position.x > 150))
         {
 
             GameManager.instance.PlayerFinished();
             Destroy(gameObject);
         }
 
+        OnMoveLeft();
 
     }
     public void Launch(Vector2 vector)
     {
         rb.bodyType = RigidbodyType2D.Dynamic;
         rb.AddForce(vector *5 , ForceMode2D.Impulse);
+    }
+
+    //destroy player when it moves out of left side of the screen
+    private void OnMoveLeft()
+    {
+        if(transform.position.x < leftExtreme)
+        {
+            GameManager.instance.PlayerFinished();
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Checks if the ball touches the ground and is in slow speed or it touches the ground behind the obstacles
+        if ((collision.gameObject.CompareTag("ground") && rb.velocity.magnitude < 0.5f) || collision.gameObject.CompareTag("ground") && transform.position.x > 100)
+        {
+            // Handle collision with an object tagged as "Obstacle"
+            Debug.Log("Collided with an obstacle!");
+            GameManager.instance.PlayerFinished();
+            Destroy(gameObject);
+            // Implement your collision handling logic here
+        }
     }
 }
